e360abd36b3eba6bdb71d6e528e9a8ccd8078794
Rodrigo Barrios <rodrigobarrios_10@hotmail.com>
2025-10-27 20:45:40 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function parOimpar (){
+    let numero = prompt("Ingrese un numero");
+    if (numero % 2 == 0){
+        alert("El numero es par");
+    }else{
+        alert("El numero es impar");
+    }
+}
+
+parOimpar(); 
