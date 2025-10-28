a94cd1b5d042ed176ceeebe847907fad541f23a0
Rodrigo Barrios <rodrigobarrios_10@hotmail.com>
2025-10-28 02:02:08 -0300

    funciones -recibir pedido y -calcular total

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const Menu_cafe = [
     { id:4, nombre: "Mocha", precio: 500 },
 ];
 
+//Mostrar menú en consola
 console.log(`--- Bienvenido a ${Nombre_Tienda} ---`);
 console.log("Menú:");
 Menu_cafe.forEach(cafe => {
@@ -18,3 +19,45 @@ Menu_cafe.forEach(cafe => {
 });
 
 console.log("---------------------------------");
+
+//Función para recibir pedido
+function recibirPedido(){
+    let idCafe = 0;
+    let cantidad = 0;
+
+    let entradaValida = false;
+    while(!entradaValida){
+        let entrada = prompt(
+            `Bienvenido a ${Nombre_Tienda}!/n/n`+
+            `Ingrese el ID del café que desea ordenar (1 al 4)`
+        );
+    
+        idcafe = parseInt(entrada);
+
+        const cafeSeleccionado = Menu_cafe.find(cafe => cafe.id === idCafe);
+        if (cafeSeleccionado) {
+            cantidad = paseInt(prompt(`Ha seleccionado ${cafeSeleccinado.nombre}./nPor favor, ingrese la cantidad deseada:`));
+        
+            if (cantidad > 0){
+                entradaValida = true;
+            } else {
+                alert("La cantidad debe ser mayor a cero. Intente nuevamente.");
+            }
+        } else{
+            alert("ID de café no valido. Por favor ingrese un número del 1 al 4");
+        }
+    }
+
+    return { 
+        idcafe: idCafe,
+        cantidad: cantidad
+    };
+}
+
+//Función para calcular total
+function calcularTotal(pedido){
+    const cafeElegido = Menu_cafe.find(cafe => cafe.id === pedido.idCafe);
+    let subtotal = cafeElegido.precio * pedido.cantidad;
+
+
+}
