bc0c4b67100846e3e4e6e71304771b15861f5459
Rodrigo Barrios <rodrigobarrios_10@hotmail.com>
2025-10-27 21:41:24 -0300

    Cafe-JS

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-function parOimpar (){
-    let numero = prompt("Ingrese un numero");
-    if (numero % 2 == 0){
-        alert("El numero es par");
-    }else{
-        alert("El numero es impar");
-    }
-}
+//Constantes
+const Nombre_Tienda = "Café-JS";
+const Tarifa_Envio = 500; 
+const Envio_Gratis = 1000;
 
-parOimpar(); 
+//Array menú
+const Menu_cafe = [
+    { id:1, nombre: "Espresso", precio: 300 },
+    { id:2, nombre: "Cappuccino", precio: 400 },
+    { id:3, nombre: "Latte", precio: 450 },
+    { id:4, nombre: "Mocha", precio: 500 },
+];
+
+console.log(`--- Bienvenido a ${Nombre_Tienda} ---`);
+console.log("Menú:");
+Menu_cafe.forEach(cafe => {
+    console.log(`ID ${cafe.id}: ${cafe.nombre} - $${cafe.precio}`);
+});
+
+console.log("---------------------------------");
